9a4e8fa96ae72a9468f2cd103f2b6f5baebc37c1
JuanGuillenA <146882347+JuanGuillenA@users.noreply.github.com>
2026-06-21 16:36:49 -0500

    Update Swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,17 +1,24 @@
 import swaggerUi from 'swagger-ui-express';
+const swaggerJSDoc = require('swagger-jsdoc');
 import { Application } from 'express';
 
-const swaggerDocument = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Super Store API',
-        version: '1.0.0',
-        description: 'Documentación oficial de los servicios del supermercado'
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Minimarket API',
+            version: '1.0.0',
+            description: 'Documentación oficial de los servicios del supermercado'
+        },
     },
-    paths: {}
+    // Aquí le decimos a Swagger en qué archivos están tus comentarios
+    apis: ['./src/routes/*.ts'] 
 };
 
+// Generamos la especificación escaneando las rutas
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
 export const setupSwagger = (app: Application) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     console.log('Swagger documentation available at /api-docs');
 };
