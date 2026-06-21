2bfde5b211c1521e64d8fc730e6f76bb9e2ca8b3
JuanGuillenA <146882347+JuanGuillenA@users.noreply.github.com>
2026-06-20 23:14:17 -0500

    implementacion de conexion real a base de datos con mongoose

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,17 @@
-export const connectDatabase = async () => {
+import mongoose from 'mongoose';
+
+export const connectDatabase = async (): Promise<void> => {
     try {
+        const dbUri = process.env.DATABASE_URL;
         
+        if (!dbUri) {
+            throw new Error('La variable DATABASE_URL no está definida en el archivo .env');
+        }
+
+        await mongoose.connect(dbUri);
         console.log('Database connection initialized successfully.');
     } catch (error) {
         console.error('Database connection failed:', error);
-        process.exit(1);
+        process.exit(1); 
     }
 };
