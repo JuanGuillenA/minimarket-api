8a46f5b6e942440163a95153105e261a5fc29813
Walther Sebastian Verdugo Cabrera <116776936+verdugong@users.noreply.github.com>
2026-06-21 21:38:53 -0500

    db: implementacion de consultas al repositorio de accesos

---
+++ src/repositories/access/Access.repository.ts
@@ -0,0 +1,24 @@
+import Role from '../../models/access/Roles.model';
+import User from '../../models/access/Users.model';
+
+export class AccessRepository {
+    // Consultas de Roles
+    async fetchActiveRoles() {
+        return await Role.find({ isActive: true });
+    }
+
+    async saveRole(roleData: any) {
+        const newRole = new Role(roleData);
+        return await newRole.save();
+    }
+
+    // Consultas de Usuarios
+    async fetchAllUsers() {
+        return await User.find({}).populate('roleId', 'roleName description');
+    }
+
+    async saveUser(userData: any) {
+        const newUser = new User(userData);
+        return await newUser.save();
+    }
+}
